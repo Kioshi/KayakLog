5b6cff64774ad2e9f8717625d2e3c62f60b70f84
Jakob John List Tøth <jakob2botg@gmail.com>
2018-11-16 08:24:08 +0100

    LocationManager added

--- app/src/main/java/cz/martinek/stepan/kayaklog/TripActivity.kt
+++ app/src/main/java/cz/martinek/stepan/kayaklog/TripActivity.kt
@@ -1,12 +1,53 @@
 package cz.martinek.stepan.kayaklog
 
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import android.util.Log
 
 class TripActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_trip)
+
+        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        val locationListener = object : LocationListener {
+
+            override fun onLocationChanged(location: Location) {
+                // Called when a new location is found by the network location provider.
+                // makeUseOfNewLocation(location)
+            val loc = location.toString()
+
+                // Print Location
+                Log.e("Location", loc)
+
+            }
+
+            override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {
+            }
+
+            override fun onProviderEnabled(provider: String) {
+            }
+
+            override fun onProviderDisabled(provider: String) {
+            }
+        }
+
+// Register the listener with the Location Manager to receive location updates
+        if (ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0f, locationListener)
+        }
     }
 }
