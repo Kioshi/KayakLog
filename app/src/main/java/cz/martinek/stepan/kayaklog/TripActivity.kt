85fa5683cdfd10e795efe56865ce16ed44c9e8e3
Jakob John List Tøth <jakob2botg@gmail.com>
2018-11-20 16:37:59 +0100

    GPS working (locationListener)

--- app/src/main/java/cz/martinek/stepan/kayaklog/TripActivity.kt
+++ app/src/main/java/cz/martinek/stepan/kayaklog/TripActivity.kt
@@ -1,58 +1,64 @@
 package cz.martinek.stepan.kayaklog
 
-import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Context
-import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v4.content.ContextCompat
-import android.util.Log
+import android.widget.Button
 import android.widget.TextView
 
 class TripActivity : AppCompatActivity() {
 
 
-   @SuppressLint("MissingPermission")
+    @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_trip)
 
-       val GPSText : TextView = findViewById(R.id.GPSText) as TextView
+        val GPSText: TextView = findViewById(R.id.GPSText) as TextView
 
 
-       val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
 
-        val locationListener = object : LocationListener {
+        val startbtn = findViewById<Button>(R.id.StartTripBtn)
 
-            override fun onLocationChanged(location: Location) {
-                // Called when a new location is found by the network location provider.
+        startbtn.setOnClickListener {
 
-            val loc = location.toString()
-                val GPSText : TextView = findViewById(R.id.GPSText) as TextView
+            val locationListener = object : LocationListener {
 
-                GPSText.append("\n " + loc)
+                override fun onLocationChanged(location: Location) {
 
 
+                    val x = location.latitude
+                    val y = location.longitude
+
+                    GPSText.setText("\nYour current location: (" + x + ":" + y + ")")
+
+
+                }
+
+                override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {
+                }
+
+                override fun onProviderEnabled(provider: String) {
+                    //GPSText.setText("\nProvider Enabled")
+
+                    //GPSText.append("\nProvider Enabled")
+                }
+
+                override fun onProviderDisabled(provider: String) {
+                    //GPSText.setText("\nProvider Disabled")
+
+                    //GPSText.append("\nProvider Disabled")
+                }
             }
-
-            override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {
-            }
-
-            override fun onProviderEnabled(provider: String) {
-                GPSText.append("\nProvider Enabled")
-
-            }
-
-            override fun onProviderDisabled(provider: String) {
-                GPSText.append("\nProvider Disabled")
-            }
-        }
-        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0f, locationListener)
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, locationListener)
 
         }
     }
+}
+
 
