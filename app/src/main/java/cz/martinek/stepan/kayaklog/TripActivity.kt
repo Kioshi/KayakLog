f3997d188bf62788ae2ab1fb55547fd65a61b31e
MetalFreak666 <egonoras@hotmail.com>
2018-11-28 18:52:46 +0100

    Map does not work in TripActivity

--- app/src/main/java/cz/martinek/stepan/kayaklog/TripActivity.kt
+++ app/src/main/java/cz/martinek/stepan/kayaklog/TripActivity.kt
@@ -2,81 +2,76 @@ package cz.martinek.stepan.kayaklog
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Color
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
 import android.widget.Button
 import android.widget.TextView
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.*
 import kotlinx.android.synthetic.main.activity_trip.*
 
-class TripActivity : AppCompatActivity(), LocationListener {
+class TripActivity : AppCompatActivity(), LocationListener, OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
+    override fun onMarkerClick(p0: Marker?) = false
 
 
+    private var x: Double = 0.0
+    private var y: Double = 0.0
 
-    override fun onLocationChanged(location: Location) {
+    private lateinit var map: GoogleMap
+    private lateinit var line: Polyline
 
-
-        val x = location.latitude
-        val y = location.longitude
-
-        val map = findViewById<Button>(R.id.mapButton)
-        map.setOnClickListener(){
-            val intent = Intent(this, MapActivity::class.java).apply {
-                putExtra("Lat", x.toString())
-                putExtra("Long", y.toString())
-            }
-            startActivity(intent)
-        }
-
-        //val myPlace = LatLng(x, y)
-
-        GPSText.setText("\nYour current location: (" + x + ":" + y + ")")
-
+    @SuppressLint("MissingPermission")
+    override fun onMapReady(googleMap: GoogleMap) {
 
     }
-
-    override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {
-    }
-
-    override fun onProviderEnabled(provider: String) {
-        //GPSText.setText("\nProvider Enabled")
-
-        GPSText.append("\nProvider Enabled")
-    }
-
-    override fun onProviderDisabled(provider: String) {
-        //GPSText.setText("\nProvider Disabled")
-
-        GPSText.append("\nProvider Disabled")
-    }
-
 
     @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_trip)
-
-
-
-
-
-
-    //val GPSText: TextView = findViewById(R.id.GPSText) as TextView
-
-
         val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
 
-        //val startbtn = findViewById<Button>(R.id.StartTripBtn)
-
-
+        val startButton = findViewById<Button>(R.id.startButton)
         startButton.setOnClickListener {
-
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, this)
+        }
+
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync {
 
         }
+
+    }
+
+    override fun onLocationChanged(location: Location) {
+        x = location.latitude
+        y = location.longitude
+
+
+    }
+
+    override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+
+    }
+
+    override fun onProviderEnabled(provider: String?) {
+
+    }
+
+    override fun onProviderDisabled(provider: String?) {
+
     }
 }
 
